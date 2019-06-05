b78aeb47278e5ac65017e98704a41824a6f8d790
Vitaly Lebedev <eslovo@gmail.com>
2019-06-05 15:26:58 +0300

    Varibles in Python

---
+++ 01-Variables.py
@@ -0,0 +1,23 @@
+
+x = "awesome"
+print("Python is " + x)
+
+x = "Python is "
+y = "awesome"
+z =  x + y
+print(z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = "Hello World!"
+print(len(x))
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
